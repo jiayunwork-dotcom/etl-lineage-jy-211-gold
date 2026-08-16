ebab1f3d38fa9519dc39ec8b557cfe9b55fa34a8
jiayun <jiayun_work@163.com>
2026-08-16 16:11:55 +0800

    chore: single independent bug (jy-211)

---
+++ internal/lineage/lineage.go
@@ -0,0 +1,48 @@
+package lineage
+
+import (
+	"errors"
+
+	"etl-lineage/internal/graph"
+)
+
+func Upstream(g *graph.Graph, id string) (map[string]bool, error) {
+	if !g.HasNode(id) {
+		return nil, errors.New("node not found")
+	}
+	var res map[string]bool
+	var dfs func(string)
+	dfs = func(n string) {
+		for _, from := range g.Predecessors(n) {
+			if !res[from] {
+				if res == nil {
+					res = map[string]bool{}
+				}
+				res[from] = true
+				dfs(from)
+			}
+		}
+	}
+	dfs(id)
+	delete(res, id)
+	return res, nil
+}
+
+func Downstream(g *graph.Graph, id string) (map[string]bool, error) {
+	if !g.HasNode(id) {
+		return nil, errors.New("node not found")
+	}
+	res := map[string]bool{}
+	var dfs func(string)
+	dfs = func(n string) {
+		for _, to := range g.Successors(n) {
+			if !res[to] {
+				res[to] = true
+				dfs(to)
+			}
+		}
+	}
+	dfs(id)
+	delete(res, id)
+	return res, nil
+}
